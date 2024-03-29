06fba97cf33ea7a3a83dfa49ac3a71f007a218b4
asantesylvesterkwame <intelligentson17@gmail.com>
2024-03-29 15:25:01 +0000

    setup project

--- README.md
+++
@@ -1,1 +0,0 @@
-# corpland
---
+++ corpland/App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
